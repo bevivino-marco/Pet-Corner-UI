72372346e7d642cf7d2595684067eb3a6d3234fb
Marco <s206263@studenti.polito.it>
2022-11-18 20:10:59 +0100

    adopt called

--- pet-corner-ui/src/App.js
+++ pet-corner-ui/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import React  from 'react';
 import './App.css';
 import Adopt from './adopt/adopt';
+import { useState, useEffect } from 'react';
+
+
+  const APIURL="http://localhost:8000/api/v1/animals";
+  async function getAnimals(){
+    const response = await fetch(APIURL)
+    .then(res=> res.json())
+    return response;
+
+  }
+
+
+
 
 function App() {
+  
+  const [animals, setAnimals] = useState([]);
+  
+  
+    async function callApi(){
+      const data = await getAnimals();
+      setAnimals(data);
+      }  
+
+
+
+  useEffect(() => {
+
+    callApi();
+    return () => {
+      
+    }
+  },[])
+
+
   return (
     <>
     <header className='p-3'>
       <div className='container'>
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
           <i>LOGO
             <svg></svg>
           </i>
           <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            <li class="nav-item"><a href="#" class="nav-link link-dark px-2 active " aria-current="page">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Features</a></li>
-            <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Pricing</a></li>
-            <li class="nav-item"><a href="#" class="nav-link link-dark px-2">FAQs</a></li>
-            <li class="nav-item"><a href="#" class="nav-link link-dark px-2">About</a></li>
+            <li className="nav-item"><a  class="nav-link link-dark px-2 active " aria-current="page" >Home</a></li>
+            <li className="nav-item"><a  class="nav-link link-dark px-2">Animal sitting</a></li>
+            <li className="nav-item"><a  class="nav-link link-dark px-2">Pet threapy</a></li>
+            <li className="nav-item"><a  class="nav-link link-dark px-2">Animal training</a></li>
           </ul>
-            <i class="bi bi-person-circle"></i>
+            <i className="bi bi-person-circle"></i>
         </div>
       </div>
     </header>
