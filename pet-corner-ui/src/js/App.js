dd7d4ecbc21ba4c83bdd6eda1ad175bf289b75b1
Alessandro <alessandro.muraro@educ.unito.it>
2022-11-20 12:07:57 +0100

    Header UI: toolbar

---
+++ pet-corner-ui/src/js/App.js
@@ -0,0 +1,15 @@
+import logo from '../logo.svg';
+import React  from 'react';
+import '../css/Layout.css';
+import Adopt from './components/adopt/adopt';
+import Header from "./components/header";
+
+function App() {
+  return (
+    <>
+      <Header />
+  </>
+  );
+}
+
+export default App;
